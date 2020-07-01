5a48c5c22c828d3b8f01bbe49710a04cfcd6e50d
guilherme granergit config --global user.name guilherme <guilherme.graner@gmail.com>
2020-07-01 08:18:33 -0300

    done without bonus

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const getBeers = require('./getBeers');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -13,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -21,5 +21,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  const beerList = await getBeers();
+  console.log(beerList[0]);
+  res.render('beers', { beers: beerList });
+});
+
+app.get('/random-beers', async (req, res) => {
+  const beerList = await getBeers();
+  res.render('random-beers');
+});
+
+// app.get('/beers/:beerId', async (req, res) => {
+//   try{
+//     const beerList = await getBeers();
+//     res.render('beer-details', { beers: beerList[req.params.beerId-1] });
+//   } catch (err) {
+//       console.log(err)
+//   }
+// });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
